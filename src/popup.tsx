7dd5e9e2483e398829c647fe67ea82d2e0db6abe
michaelPotter <mpotter.dev@gmail.com>
2024-06-24 09:59:13 -0700

    feat: add the archive page

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,76 +1,64 @@
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+import ReactDOM from 'react-dom';
+import { observer } from "mobx-react";
+
+import _ from 'lodash';
+
+import Window from './js/model/Window';
+import WindowManager from './js/WindowManager';
+import WindowComponent from './components/Window';
+import PopupStore from './popupStore';
 
 // Pull in the styles ...
 import './scss/root.scss';
 
-import Window from './js/model/Window';
-import WindowManager from './js/WindowManager';
-import _ from 'lodash';
+const App = observer(() => {
+	let windowsMap = WindowManager.windows;
+	let windows = Object.values(windowsMap).sort(Window.accessCompare);
 
-import ReactDOM from 'react-dom';
+	return (
+		<>
+			<div id="header">
+				<a onClick={() => PopupStore.setPage("alltabs")}>all tabs</a>
+				{" | "}
+				<a onClick={() => PopupStore.setPage("archive")}>archive</a>
+				<i id="popout_button" onClick={open_in_window} className="material-icons">open_in_new</i>
+				<i id="refresh_button" onClick={() => location.reload()} className="material-icons">refresh</i>
+			</div>
+			<div id="body">
+			{ PopupStore.page == "alltabs"
+				&&
+					windows.map(w =>
+						<WindowComponent
+							window={w}
+							tabs={w.tabs}
+							key={w.id}
+							onCloseClick={() => WindowManager.closeWindow(w.id)}
+							/>
+				   )
+				||
+					<p>
+						The Archive
+					</p>
+			}
+			</div>
+		</>
+	)
+})
 
-import WindowComponent from './components/Window';
-import PopupStore from './popupStore';
-
-document.addEventListener('DOMContentLoaded', function() {
-	reactMain();
-});
-
-async function reactMain() {
-
+document.addEventListener('DOMContentLoaded', async function() {
 	await WindowManager.waitForPopulated();
-
-	// /*
-	//  * A map that holds reference to every tab, for easier access. key'd by tabId
-	//  * This lets you easily access and modify a tab without having to search through the list,
-	//  */
-	// let tabsMap = _(windows).flatMap(w => w.tabs).keyBy((tab: Tab) => tab.id ?? -1).value()
-
 
 	/*
 	 * Create a closure to re-render, allowing access the window/tab data structures
 	 */
 	function render() {
-		let windowsMap = WindowManager.windows;
-		let windows = Object.values(windowsMap).sort(Window.accessCompare);
-
-		let main = (
-			<>
-				<div id="header">
-					<a onClick={() => PopupStore.setPage("alltabs")}>all tabs</a>
-					<a onClick={() => PopupStore.setPage("archive")}>archive</a>
-					<i id="popout_button" onClick={open_in_window} className="material-icons">open_in_new</i>
-					<i id="refresh_button" onClick={() => location.reload()} className="material-icons">refresh</i>
-				</div>
-				<div id="body">
-				{ PopupStore.page == "alltabs"
-					&&
-						windows.map(w =>
-							<WindowComponent
-								window={w}
-								tabs={w.tabs}
-								key={w.id}
-								onCloseClick={() => WindowManager.closeWindow(w.id)}
-								/>
-					   )
-					||
-						<p>
-							The Archive
-						</p>
-				}
-				</div>
-			</>
-		)
-
-		ReactDOM.render(main, document.getElementById('main'));
+		ReactDOM.render(<App/>, document.getElementById('main'));
 	}
 
 	WindowManager.onTabChange(render);
 
 	render();
-
-}
+});
 
 function open_in_window() {
 	var win: browser.windows._CreateCreateData = {
