77864370010439c2948e55c388fca03a809cdca8
michaelPotter <mpotter.dev@gmail.com>
2024-08-10 15:24:59 -0700

    Move cyrb53 hash util to own file

--- src/js/model/archivedWindowGroup/ArchivedWindowGroupDAO.ts
+++ src/js/model/archivedWindowGroup/ArchivedWindowGroupDAO.ts
@@ -1,3 +1,4 @@
+import { cyrb53 } from "../../util";
 import { ArchivedTab, ArchivedWindow, ArchivedWindowGroup, ArchivedWindowGroupData } from "./ArchivedWindowGroup";
 import { ArchivedWindowGroupExport, ExportedArchivedWindowGroup, ExportedTab, ExportedWindow } from "./ExportedArchivedWindowGroup";
 
@@ -171,19 +172,3 @@ export default class ArchivedWindowGroupDAO {
 	}
 
 }
-
-
-const cyrb53 = (str: string, seed = 0) => {
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for(let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
-    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
-    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-
-    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
-};
